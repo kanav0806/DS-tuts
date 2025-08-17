1221b10da2cf36cddb6f047ce8e555bf30e2ffcf
kanav0806 <kbhardwaj1_be24@thapar.edu>
2025-08-18 00:03:03 +0530

    Add files via upload

---
+++ q5_sum_row_col.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], rows, cols;
+    int i, j, rowSum, colSum;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nSum of each row:\n");
+    for (i = 0; i < rows; i++) {
+        rowSum = 0;
+        for (j = 0; j < cols; j++) {
+            rowSum += a[i][j];
+        }
+        printf("Row %d sum = %d\n", i + 1, rowSum);
+    }
+
+    printf("\nSum of each column:\n");
+    for (j = 0; j < cols; j++) {
+        colSum = 0;
+        for (i = 0; i < rows; i++) {
+            colSum += a[i][j];
+        }
+        printf("Column %d sum = %d\n", j + 1, colSum);
+    }
+
+    return 0;
+}
